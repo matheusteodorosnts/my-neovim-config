aed7f3ca27c3974112081f65a9a296157c261c96
matheusteodorosnts <matheus.teodoro.snts@gmail.com>
2026-08-01 03:17:08 -0300

    feat: add telescope

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -18,6 +18,15 @@ return {
 
       dashboard.section.header.val = vim.split(logo, "\n")
 
+      dashboard.section.buttons.val = {
+	dashboard.button("n", "󰈔  " .. " New file", [[<cmd> ene <BAR> startinsert <cr>]]),
+	dashboard.button("f", "  " .. " Find file", "<cmd> Telescope find_files <cr>"),
+	dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
+        dashboard.button("g", "  " .. " Find text", "<cmd> Telescope live_grep <cr>"),
+        dashboard.button("c", " " .. " Config", "<cmd> Telescope find_files cwd=" .. vim.fn.stdpath("config") .. " <cr>"),
+        dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
+      }
+
       require("alpha").setup(dashboard.opts)
     end,
   },
